baa98ce88bda1deca047544de3991f98a237445c
shinjw4929 <sjw4929@gmail.com>
2024-09-17 23:23:27 +0900

    self_test_all_passed

--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -47,30 +47,30 @@ class FunSetSuite extends FunSuite {
     assert(1 + 2 === 3)
   }
 
-  
+
   import FunSets._
 
   test("contains is implemented") {
     assert(contains(x => true, 100))
   }
-  
+
   /**
    * When writing tests, one would often like to re-use certain values for multiple
    * tests. For instance, we would like to create an Int-set and have multiple test
    * about it.
-   * 
+   *
    * Instead of copy-pasting the code for creating the set into every test, we can
    * store it in the test class using a val:
-   * 
+   *
    *   val s1 = singletonSet(1)
-   * 
+   *
    * However, what happens if the method "singletonSet" has a bug and crashes? Then
    * the test methods are not even executed, because creating an instance of the
    * test class fails!
-   * 
+   *
    * Therefore, we put the shared values into a separate trait (traits are like
    * abstract classes), and create an instance inside each test method.
-   * 
+   *
    */
 
   trait TestSets {
@@ -82,15 +82,15 @@ class FunSetSuite extends FunSuite {
   /**
    * This test is currently disabled (by using "ignore") because the method
    * "singletonSet" is not yet implemented and the test would fail.
-   * 
+   *
    * Once you finish your implementation of "singletonSet", exchange the
    * function "ignore" by "test".
    */
-  ignore("singletonSet(1) contains 1") {
-    
+  test("singletonSet(1) contains 1") {
+
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
-     * to the values "s1" to "s3". 
+     * to the values "s1" to "s3".
      */
     new TestSets {
       /**
@@ -101,12 +101,86 @@ class FunSetSuite extends FunSuite {
     }
   }
 
-  ignore("union contains all elements") {
+  test("union contains all elements") {
     new TestSets {
       val s = union(s1, s2)
+      printSet(s)
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
     }
   }
+
+
+  test("test intersect") {
+    new TestSets {
+      val union12 = union(s1, s2)
+      val union23 = union(s2, s3)
+      val inter12_23 = intersect(union12, union23)
+      printSet(inter12_23)
+      assert(!contains(inter12_23, 1), "Intersect 1")
+      assert(contains(inter12_23, 2), "Intersect 2")
+      assert(!contains(inter12_23, 3), "Intersect 3")
+    }
+  }
+
+
+  test("test diff") {
+    new TestSets {
+      val union12 = union(s1, s2)
+      val union23 = union(s2, s3)
+      val diff12_23 = diff(union12, union23)
+      printSet(diff12_23)
+      assert(contains(diff12_23, 1), "Diff 1")
+      assert(!contains(diff12_23, 2), "Diff 2")
+      assert(!contains(diff12_23, 3), "Diff 3")
+    }
+  }
+
+  test("test filter") {
+    new TestSets {
+      val union12 = union(s1, s2)
+      val union23 = union(s2, s3)
+      val union123 = union(union12, union23)
+      val filter123 = filter(union123, x => (x % 2) == 1)
+      printSet(filter123)
+      assert(contains(filter123, 1), "Filter 1")
+      assert(!contains(filter123, 2), "Filter 2")
+      assert(contains(filter123, 3), "Filter 3")
+    }
+  }
+
+  test("test forall") {
+    new TestSets {
+      val union13 = union(s1, s3)
+      val forall123 = forall(union13, x => (x % 2) == 1)
+      assert(forall123, "Forall")
+    }
+  }
+
+  test("test exists") {
+    new TestSets {
+      val union12 = union(s1, s2)
+      val union23 = union(s2, s3)
+      val union123 = union(union12, union23)
+      val exists123 = exists(union123, x => (x % 2) == 1)
+      assert(exists123, "Exists")
+    }
+  }
+
+  test("test map") {
+    new TestSets {
+      val union12 = union(s1, s2)
+      val union23 = union(s2, s3)
+      val union123 = union(union12, union23)
+      val map123 = map(union123, x => x * x)
+      printSet(map123)
+      assert(contains(map123, 1), "Map 1")
+      assert(!contains(map123, 2), "Map 2")
+      assert(!contains(map123, 3), "Map 3")
+      assert(contains(map123, 4), "Map 4")
+      assert(contains(map123, 9), "Map 9")
+    }
+  }
+
 }
